483fb6e81e465705fec029b032a34fa92c15ecb1
Hải Long <longdh2.dev@gmail.com>
2025-05-15 19:06:50 +0700

    fix: load .env

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <?php
-require 'vendor/autoload.php';
 define('APP_PATH', __DIR__ . '/');
+require 'vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 require_once 'App/Routings/web.php';
 require_once 'App/Core/BaseController.php';
 require_once 'App/AppHandle.php';
 require_once 'App/database/DB.php';
 require_once 'App/Core/helper.php';
-
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
 
 
 use App\Appilaction;
